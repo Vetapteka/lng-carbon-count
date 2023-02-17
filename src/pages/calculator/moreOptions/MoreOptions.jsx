9222783d4eb42dbc075e79ee3b0f5c84a620bb1f
vetapteka <cvetka_top@mail.ru>
2023-02-17 12:17:29 +0300

    add MoreOptions

--- src/pages/calculator/moreOptions/MoreOptions.jsx
+++ src/pages/calculator/moreOptions/MoreOptions.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import MoreOptionsLayout from './MoreOptionsLayout';
+import Button from '../../../components/common/UI/Button/Button';
+import { buttonRoles } from '../../../components/common/UI/Button/ButtonRoles';
 import Form from '../../../components/common/UI/Form/Form';
+import { Size } from '../../../components/common/UI/utils';
+import {
+    generalParams,
+    calculationParams,
+    emissionIntensityParams,
+} from '../../../utils/lng-variables';
 
 const MoreOptions = () => {
-    // const 
+    const generalForm = (
+        <Form id='form' size={new Size(1, 4)} fields={generalParams} />
+    );
+    const emissionForm = (
+        <Form
+            id='form'
+            size={new Size(1, 7)}
+            fields={emissionIntensityParams}
+        />
+    );
+    const parametersForm = (
+        <Form id='form' size={new Size(1, 4)} fields={calculationParams} />
+    );
+    const button = (
+        <Button role={buttonRoles.main} type='submit' form='form'>
+            save
+        </Button>
+    );
     return (
-<></>
-        // <Form id='form'  />
-        // <Button role={buttonRoles.main} type='submit' form='form'>save</Button>
+        <MoreOptionsLayout
+            generalForm={generalForm}
+            emissionForm={emissionForm}
+            parametersForm={parametersForm}
+            button={button}
+        />
     );
 };
 
--- src/pages/calculator/moreOptions/MoreOptionsLayout.jsx
+++ src/pages/calculator/moreOptions/MoreOptionsLayout.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Section from '../../../components/common/Section';
+import classes from './MoreOptionsLayout.module.css';
 
-const MoreOptionsLayout = () => {
+const MoreOptionsLayout = ({
+    generalForm,
+    emissionForm,
+    parametersForm,
+    button,
+}) => {
     return (
-        <div>
-            
+        <div className={classes.body}>
+            <Section title='Emission intensity'>{emissionForm}</Section>
+            <Section title='General form'>{generalForm}</Section>
+            <Section title='parameters for calculation'>
+                {parametersForm}
+            </Section>
+            <div className={classes.button_grid}>{button}</div>
         </div>
     );
 };
