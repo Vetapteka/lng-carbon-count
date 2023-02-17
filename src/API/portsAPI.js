51a1eb388de0eb33c2c3bdae70f9ca287d28a775
vetapteka <cvetka_top@mail.ru>
2023-02-17 21:37:40 +0300

    add API for lng-variables

--- src/API/portsAPI.js
+++ src/API/portsAPI.js
@@ -4,3 +4,18 @@ export const fetchPorts = async () => {
     const { data } = await $host.get('./data_example/ports.json');
     return data;
 };
+
+export const fetchCalculationParams = async () => {
+    const { data } = await $host.get('./data_example/calculation_params.json');
+    return data;
+};
+
+export const fetchEmissionParams = async () => {
+    const { data } = await $host.get('./data_example/emission_params.json');
+    return data;
+};
+
+export const fetchGeneralParams = async () => {
+    const { data } = await $host.get('./data_example/general_params.json');
+    return data;
+};
