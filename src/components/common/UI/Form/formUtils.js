9132d652954c0c6ffd8a67c18bce547f8d719296
vetapteka <cvetka_top@mail.ru>
2023-02-19 21:05:30 +0300

    small fix +small style changes

--- src/components/common/UI/Form/formUtils.js
+++ src/components/common/UI/Form/formUtils.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import Select from '../Select';
 import Input from '../Input/Input';
-import InputRadio from '../Input/InputRadio';
 import { InputItem } from '../../../../utils/formUtils/formItems/InputItem';
-import { SelectItem } from '../../../../utils/formUtils/formItems/SelectItem';
-import { InputRadioItem } from '../../../../utils/formUtils/formItems/InputRadioItem';
 
 export const getTemplate = (size) => {
     return `repeat(${size.rowCount}, 1fr)/repeat(${size.columnCount}, 1fr)`;
